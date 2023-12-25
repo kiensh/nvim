38297d7b8bfbb45bcb358d79fd6ae5b0c34c58be
duckien2346 <duckien2346@gmail.com>
2023-12-25 15:10:31 +0700

    fix sign column lspconfig

--- lua/uit/plugins/lsp/lspconfig/nvim-lspconfig.lua
+++ lua/uit/plugins/lsp/lspconfig/nvim-lspconfig.lua
@@ -26,7 +26,7 @@ return {
         -- Change the Diagnostic symbols in the sign column (gutter)
         local signs = { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
         for type, icon in pairs(signs) do
-            local hl = 'diagnosticsign' .. type
+            local hl = 'DiagnosticSign' .. type
             vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
         end
 
