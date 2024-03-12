7465429fd695959e4c6e28724a8c0245d2648a1c
duckien2346 <duckien2346@gmail.com>
2024-03-12 23:18:27 +0700

    feat: update code action

--- lua/plugins/lsp/lspconfig/nvim-lspconfig.lua
+++ lua/plugins/lsp/lspconfig/nvim-lspconfig.lua
@@ -1,3 +1,21 @@
+local codeAction = {
+    dynamicRegistration = false,
+    codeActionLiteralSupport = {
+        codeActionKind = {
+            valueSet = {
+                "",
+                "quickfix",
+                "refactor",
+                "refactor.extract",
+                "refactor.inline",
+                "refactor.rewrite",
+                "source",
+                "source.organizeImports",
+            },
+        },
+    },
+}
+
 return {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
@@ -8,21 +26,21 @@ return {
         "j-hui/fidget.nvim",
     },
     config = function()
-        -- import lspconfig plugin
         local lspconfig = require("lspconfig")
         -- local util = require("lspconfig.util")
         require("lspconfig.ui.windows").default_options.border = "double"
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+        capabilities.textDocument.codeAction = codeAction
         lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
             on_attach = require("plugins.lsp.lspconfig.on_attach"),
-            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            capabilities = capabilities,
             handlers = {
                 ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
                 ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
             },
         })
 
-        -- Change the Diagnostic symbols in the sign column (gutter)
         local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
         for type, icon in pairs(signs) do
             local hl = "DiagnosticSign" .. type
@@ -30,7 +48,6 @@ return {
         end
 
         local list_lsp = require("plugins.lsp.lspconfig.list-lsp")
-
         for k, v in pairs(list_lsp) do
             lspconfig[k].setup(v)
         end
