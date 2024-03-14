c5ac21fe4e642e234c945574645e136739274509
kiensh <91678288+kiensh@users.noreply.github.com>
2024-03-14 19:35:37 +0700

    update: format trouble.lua

--- lua/plugins/lsp/trouble.lua
+++ lua/plugins/lsp/trouble.lua
@@ -1,36 +1,11 @@
 return {
     "folke/trouble.nvim",
     keys = {
-        {
-            "<leader>xx",
-            function()
-                require("trouble").toggle()
-            end,
-        },
-        {
-            "<leader>xw",
-            function()
-                require("trouble").toggle("workspace_diagnostics")
-            end,
-        },
-        {
-            "<leader>xd",
-            function()
-                require("trouble").toggle("document_diagnostics")
-            end,
-        },
-        {
-            "<leader>xq",
-            function()
-                require("trouble").toggle("quickfix")
-            end,
-        },
-        {
-            "<leader>xl",
-            function()
-                require("trouble").toggle("loclist")
-            end,
-        },
+        { "<leader>xx", function() require("trouble").toggle() end, },
+        { "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end, },
+        { "<leader>xd", function() require("trouble").toggle("document_diagnostics") end, },
+        { "<leader>xq", function() require("trouble").toggle("quickfix") end, },
+        { "<leader>xl", function() require("trouble").toggle("loclist") end, },
         -- { "gR", function() require("trouble").toggle("lsp_references") end, },
     },
     opts = {
