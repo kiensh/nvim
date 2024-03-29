fff11cf1a4b81e64f294ecc1775593669ec3823b
duckien2346 <duckien2346@gmail.com>
2024-03-29 22:33:03 +0700

    feat: refactoring gitsigns and harpoon

--- lua/plugins/pretitier/gitsigns.lua
+++ lua/plugins/pretitier/gitsigns.lua
@@ -9,48 +9,32 @@ local on_attach = function(bufnr)
 
     -- Navigation
     map("n", "]h", function()
-        if vim.wo.diff then
-            return "]h"
-        end
-
-        vim.schedule(function()
-            gs.next_hunk()
-        end)
+        if vim.wo.diff then return "]h" end
+        vim.schedule(function() gs.next_hunk() end)
         return "<Ignore>"
     end, { expr = true })
 
     map("n", "[h", function()
-        if vim.wo.diff then
-            return "[h"
-        end
-        vim.schedule(function()
-            gs.prev_hunk()
-        end)
+        if vim.wo.diff then return "[h" end
+        vim.schedule(function() gs.prev_hunk() end)
         return "<Ignore>"
     end, { expr = true })
 
     -- Actions
     map("n", "<leader>hs", gs.stage_hunk)
-    map("n", "<leader>hr", gs.reset_hunk)
-    map("v", "<leader>hs", function()
-        gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-    end)
-    map("v", "<leader>hr", function()
-        gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-    end)
+    map("v", "<leader>hs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end)
     map("n", "<leader>hS", gs.stage_buffer)
+    map("n", "<leader>hx", gs.reset_hunk)
+    map("v", "<leader>hx", function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end)
+    map("n", "<leader>hX", gs.reset_buffer)
+
     map("n", "<leader>hu", gs.undo_stage_hunk)
-    map("n", "<leader>hR", gs.reset_buffer)
     map("n", "<leader>hp", gs.preview_hunk)
-    map("n", "<leader>hb", function()
-        gs.blame_line({ full = true })
-    end)
-    map("n", "<leader>tb", gs.toggle_current_line_blame)
+    map("n", "<leader>hb", function() gs.blame_line({ full = true }) end)
     map("n", "<leader>hd", gs.diffthis)
-    map("n", "<leader>hD", function()
-        gs.diffthis("~")
-    end)
-    map("n", "<leader>td", gs.toggle_deleted)
+    map("n", "<leader>hD", function() gs.diffthis("~") end)
+    -- map("n", "<leader>tb", gs.toggle_current_line_blame)
+    -- map("n", "<leader>td", gs.toggle_deleted)
 
     -- Text object
     map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
