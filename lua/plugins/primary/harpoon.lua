557ab76835cb6828d2bdbbac64f429876c11cb4c
duckien2346 <duckien2346@gmail.com>
2024-06-25 22:55:03 +0700

    update: breaking changes in trouble and harpoon

--- lua/plugins/primary/harpoon.lua
+++ lua/plugins/primary/harpoon.lua
@@ -20,7 +20,7 @@ return {
         local harpoon = require("harpoon")
         harpoon:setup()
 
-        vim.keymap.set("n", "å", function() harpoon:list():append() end)
+        vim.keymap.set("n", "å", function() harpoon:list():add() end)
         vim.keymap.set("n", "µ", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
         vim.keymap.set("n", "˜", function() harpoon:list():next() end)
