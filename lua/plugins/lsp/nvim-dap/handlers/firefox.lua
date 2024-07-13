f0d1be4181a6d229ed2695e6507b27012b9be878
kiensh <91678288+kiensh@users.noreply.github.com>
2024-07-13 11:18:57 +0700

    feat: add firefox dap handler

---
+++ lua/plugins/lsp/nvim-dap/handlers/firefox.lua
@@ -0,0 +1,21 @@
+return function(config)
+    local handle = io.popen(MY_OS.isWindows()
+        and "Get-Command firefox"
+        or "which firefox"
+    )
+    ---@cast handle -nil
+    local firefox_path = handle:read("*a"):gsub("\n", "")
+    handle:close()
+    config.configurations = {
+        {
+            firefoxExecutable = firefox_path,
+            name = "Firefox: Debug",
+            reAttach = true,
+            request = "launch",
+            type = "firefox",
+            url = "http://localhost:4200/#",
+            webRoot = "${workspaceFolder}",
+        },
+    }
+    require("mason-nvim-dap").default_setup(config)
+end
--- lua/plugins/lsp/nvim-dap/mason-nvim-dap.lua
+++ lua/plugins/lsp/nvim-dap/mason-nvim-dap.lua
@@ -4,11 +4,12 @@ return {
     dependencies = { "williamboman/mason.nvim" },
     opts = {
         ensure_installed = {},
-        automatic_installation = false,
+        automatic_installation = false, -- only install from dap not hanlders
         handlers = {
             function(config) require("mason-nvim-dap").default_setup(config) end,
             coreclr = require("plugins.lsp.nvim-dap.handlers.coreclr"),
             bash = require("plugins.lsp.nvim-dap.handlers.bashdb"),
+            firefox = require("plugins.lsp.nvim-dap.handlers.firefox"),
         },
     },
 }
