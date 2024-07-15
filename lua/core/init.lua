7a8c0c8c719af7a4b2f01582ade1bebb8686d76f
kiensh <91678288+kiensh@users.noreply.github.com>
2024-07-15 19:25:53 +0700

    update: move MY_OS from core.key to core

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,4 +1,19 @@
+local home = vim.fn.expand("~")
+MY_OS = {
+    value = "",
+    isWindows = function() return MY_OS.value == "windows" end,
+    isMac = function() return MY_OS.value == "mac" end,
+    isLinux = function() return MY_OS.value == "linux" end,
+}
+if vim.startswith(home, "/home/") then
+    MY_OS.value = "linux"
+elseif vim.startswith(home, "/Users/") then
+    MY_OS.value = "mac"
+else
+    MY_OS.value = "windows"
+end
+
+require("core.intro")
 require("core.keys")
 require("core.keymaps")
 require("core.options")
-require("core.intro")
--- lua/core/keys.lua
+++ lua/core/keys.lua
@@ -157,21 +157,6 @@ KEYS.alt = {
     a5 = "<A-5>",
 }
 
-local home = vim.fn.expand("~")
-MY_OS = {
-    value = "",
-    isWindows = function() return MY_OS.value == "windows" end,
-    isMac = function() return MY_OS.value == "mac" end,
-    isLinux = function() return MY_OS.value == "linux" end,
-}
-if vim.startswith(home, "/home/") then
-    MY_OS.value = "linux"
-elseif vim.startswith(home, "/Users/") then
-    MY_OS.value = "mac"
-else
-    MY_OS.value = "windows"
-end
-
 if MY_OS.isMac() then
     KEYS.alt = {
         a = "å",
