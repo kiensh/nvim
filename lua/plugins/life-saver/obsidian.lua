b13e9cbb7b1cbb0d0ba0c56191832e34bedca828
kiensh <91678288+kiensh@users.noreply.github.com>
2024-07-19 20:00:05 +0700

    backup: 2024-07-19 20:00:05   - lua/plugins/life-saver/obsidian.lua

--- lua/plugins/life-saver/obsidian.lua
+++ lua/plugins/life-saver/obsidian.lua
@@ -8,9 +8,7 @@ return {
     },
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "hrsh7th/nvim-cmp",
         "nvim-telescope/telescope.nvim",
-        "nvim-treesitter/nvim-treesitter",
     },
     opts = {
         workspaces = {
