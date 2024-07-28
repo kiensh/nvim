ddf5d8bc94b823bdb703beeb6f280f780ee83bde
kiensh <91678288+kiensh@users.noreply.github.com>
2024-07-28 20:37:21 +0700

    fix: lazy loading of nvim-dap

--- lua/plugins/lsp/nvim-dap/keymaps.lua
+++ lua/plugins/lsp/nvim-dap/keymaps.lua
@@ -6,35 +6,32 @@ local toggle_sidebar = function(sidebar)
         vim.api.nvim_set_current_win(win)
     end
 end
-return function()
-    local dap = require("dap")
-    local widgets = require("dap.ui.widgets")
-    local winopts = {
-        width = 50,
-        number = true,
-        relativenumber = true,
-    }
-    local frames_sidebar = widgets.sidebar(widgets.frames, winopts)
-    local scopes_sidebar = widgets.sidebar(widgets.scopes, winopts)
 
-    return {
-        { KEYS.f5, dap.continue },
-        { KEYS.f10, dap.step_over },
-        { KEYS.f11, dap.step_into },
-        { KEYS.f12, dap.step_out },
+local dap = require("dap")
+local widgets = require("dap.ui.widgets")
+local winopts = {
+    width = 50,
+    number = true,
+    relativenumber = true,
+}
+local frames_sidebar = widgets.sidebar(widgets.frames, winopts)
+local scopes_sidebar = widgets.sidebar(widgets.scopes, winopts)
 
-        { KEYS.leader.d.c, dap.close },
-        { KEYS.leader.d.j, dap.down },
-        { KEYS.leader.d.k, dap.up },
+vim.keymap.set("n", KEYS.f5, dap.continue)
+vim.keymap.set("n", KEYS.f10, dap.step_over)
+vim.keymap.set("n", KEYS.f11, dap.step_into)
+vim.keymap.set("n", KEYS.f12, dap.step_out)
 
-        { KEYS.leader.b, dap.toggle_breakpoint },
-        { KEYS.leader.B, dap.set_breakpoint },
+vim.keymap.set("n", KEYS.leader.d.c, dap.close)
+vim.keymap.set("n", KEYS.leader.d.j, dap.down)
+vim.keymap.set("n", KEYS.leader.d.k, dap.up)
 
-        { KEYS.leader.d.r, dap.repl.toggle },
-        { KEYS.leader.d.l, dap.run_last },
-        { KEYS.leader.d.h, widgets.hover, mode = { "n", "v" } },
-        -- { KEYS.leader.d.p, widgets.preview, mode = { "n", "v" } },
-        { KEYS.leader.d.f, function() toggle_sidebar(frames_sidebar) end },
-        { KEYS.leader.d.s, function() toggle_sidebar(scopes_sidebar) end },
-    }
-end
+vim.keymap.set("n", KEYS.leader.b, dap.toggle_breakpoint)
+vim.keymap.set("n", KEYS.leader.B, dap.set_breakpoint)
+
+vim.keymap.set("n", KEYS.leader.d.r, dap.repl.toggle)
+vim.keymap.set("n", KEYS.leader.d.l, dap.run_last)
+vim.keymap.set({"n", "v"}, KEYS.leader.d.h, widgets.hov)
+-- vim.keymap.set({"n", "v"}, KEYS.leader.d.p, widgets.previ)
+vim.keymap.set("n", KEYS.leader.d.f, function() toggle_sidebar(frames_sidebar) end)
+vim.keymap.set("n", KEYS.leader.d.s, function() toggle_sidebar(scopes_sidebar) end)
--- lua/plugins/lsp/nvim-dap/nvim-dap.lua
+++ lua/plugins/lsp/nvim-dap/nvim-dap.lua
@@ -15,10 +15,30 @@ vim.api.nvim_create_autocmd({ "BufWinEnter", "FileType" }, {
 return {
     "mfussenegger/nvim-dap",
     dependencies = { "jay-babu/mason-nvim-dap.nvim" },
-    keys = require("plugins.lsp.nvim-dap.keymaps"),
+    keys = {
+        KEYS.f5,
+        KEYS.f10,
+        KEYS.f11,
+        KEYS.f12,
+
+        KEYS.leader.d.c,
+        KEYS.leader.d.j,
+        KEYS.leader.d.k,
+
+        KEYS.leader.b,
+        KEYS.leader.B,
+
+        KEYS.leader.d.r,
+        KEYS.leader.d.l,
+        KEYS.leader.d.h,
+        -- KEYS.leader.d.p,
+        KEYS.leader.d.f,
+        KEYS.leader.d.s,
+    },
     config = function()
         local dap = require("dap")
         dap.set_log_level("TRACE")
         require("dap.ext.vscode").load_launchjs()
+        require("plugins.lsp.nvim-dap.keymaps")
     end,
 }
