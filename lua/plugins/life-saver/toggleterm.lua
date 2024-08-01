b00970889e0cba08a6ecf1c4532a74aa52467e3b
kiensh <91678288+kiensh@users.noreply.github.com>
2024-08-01 20:12:51 +0700

    feat: add dynamic terminal size

--- lua/plugins/life-saver/toggleterm.lua
+++ lua/plugins/life-saver/toggleterm.lua
@@ -13,7 +13,7 @@ return {
         -- size = 10,
         size = function(term)
             if term.direction == "horizontal" then
-                return 15
+                return vim.o.lines * 0.6
             elseif term.direction == "vertical" then
                 return vim.o.columns * 0.4
             end
