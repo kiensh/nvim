2eb9e2ca6fe7fcdafd264fc8d143836bf8774e48
kiensh <91678288+kiensh@users.noreply.github.com>
2025-05-24 20:00:07 +0700

    backup: 2025-05-24 20:00:06   - lua/plugins/others/avante.lua   - lua/plugins/others/copilot.lua   - lua/plugins/others/gen.lua   - lua/plugins/others/hardtime.lua

--- lua/plugins/others/copilot.lua
+++ lua/plugins/others/copilot.lua
@@ -1,34 +1,49 @@
 return {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    opts = {
-        suggestion = {
-            enabled = true,
-            auto_trigger = true,
-            debounce = 75,
-            keymap = {
-                accept = KEYS.ctrl.l,
-                accept_word = false,
-                accept_line = false,
-                next = KEYS.ctrl.rbracket,
-                -- Disabled because <C-[> is represented as <Esc> in terminal
-                -- prev = KEYS.ctrl.lbracket,
-                dismiss = KEYS.ctrl.c,
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        opts = {
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                debounce = 75,
+                keymap = {
+                    accept = KEYS.ctrl.l,
+                    accept_word = false,
+                    accept_line = false,
+                    next = KEYS.ctrl.rbracket,
+                    -- Disabled because <C-[> is represented as <Esc> in terminal
+                    -- prev = KEYS.ctrl.lbracket,
+                    dismiss = KEYS.ctrl.c,
+                },
             },
+            filetypes = {
+                yaml = true,
+                markdown = true,
+                help = false,
+                gitcommit = true,
+                gitrebase = true,
+                hgcommit = false,
+                svn = false,
+                cvs = false,
+                ["."] = true,
+            },
+            copilot_node_command = "node", -- Node.js version must be > 16.x
+            copilot_model = "gpt-4o-copilot", -- Current LSP default is gpt-35-turbo, supports gpt-4o-copilot
         },
-        filetypes = {
-            yaml = true,
-            markdown = true,
-            help = false,
-            gitcommit = true,
-            gitrebase = true,
-            hgcommit = false,
-            svn = false,
-            cvs = false,
-            ["."] = true,
+    },
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+            { "zbirenbaum/copilot.lua" }, -- or zbirenbaum/copilot.lua
+            { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+            { "stevearc/dressing.nvim" },
         },
-        copilot_node_command = "node", -- Node.js version must be > 16.x
-        copilot_model = "gpt-4o-copilot",  -- Current LSP default is gpt-35-turbo, supports gpt-4o-copilot
+        build = "make tiktoken", -- Only on MacOS or Linux
+        opts = {
+            -- model = "gpt-4o",
+            -- model = "claude-3.7-sonnet-thought",
+        },
     },
 }
