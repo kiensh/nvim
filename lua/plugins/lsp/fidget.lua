a9670ab1fe79ff81ccbe43f2b04477404a866074
kiensh <91678288+kiensh@users.noreply.github.com>
2026-01-17 00:45:45 +0700

    Revert "feat: remove fidget plugin for LSP progress"
    This reverts commit cf40ccb5cbbe515896ad8fc6d0503abc10bd6c31.

---
+++ lua/plugins/lsp/fidget.lua
@@ -0,0 +1,12 @@
+return {
+    "j-hui/fidget.nvim",
+    opts = {
+        notification = {
+            window = {
+                normal_hl = "Normal",
+                winblend = 0,
+                border = "rounded",
+            },
+        },
+    },
+}
