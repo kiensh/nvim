2e1464a32f02cba3b479e5543356530caf02c1ba
kiensh <91678288+kiensh@users.noreply.github.com>
2026-01-17 20:00:04 +0700

    backup: 2026-01-17 20:00:04   - lua/plugins/others/copilot.lua

--- lua/plugins/others/copilot.lua
+++ lua/plugins/others/copilot.lua
@@ -31,7 +31,7 @@ return {
                 cvs = false,
             },
             copilot_node_command = "node",
-            copilot_model = "gpt-4o-copilot",
+            copilot_model = "gpt-5.1",
         },
     },
     {
